3adfd7d455804f0f8b4dab895d8bf69cbdb2ce67
Anton Krynytskyi <Anton_Krynytskyi@epam.com>
2015-12-15 09:03:46 +0200

    add implementation

---
+++ src/main/java/com/epam/cdp/anton/krynytskyi/domain/model/Const.java
@@ -0,0 +1,7 @@
+package com.epam.cdp.anton.krynytskyi.domain.model;
+
+/**
+ * Created by Home on 13.12.2015.
+ */
+public class Const {
+}
---
+++ src/test/java/com/epam/cdp/anton/krynytskyi/domain/dao/store/EventDAOStoreTest.java
@@ -0,0 +1,7 @@
+package com.epam.cdp.anton.krynytskyi.domain.dao.store;
+
+/**
+ * Created by Home on 13.12.2015.
+ */
+public class EventDAOStoreTest {
+}
